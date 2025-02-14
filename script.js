53ff5510bdc4a53c2520bd13dc590ebc4208fff2
Marcell Costa <126292879+MarcellCosta31@users.noreply.github.com>
2025-02-14 00:07:21 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function validarLogin() {
+    const usuario = document.getElementById('usuario').value;
+    const senha = document.getElementById('senha').value;
+    const mensagem = document.getElementById('mensagem');
+    
+    const usuarioCorreto = "Lua";
+    const senhaCorreta = "030225";
+
+    if (usuario === usuarioCorreto && senha === senhaCorreta) {
+        window.location.href = "pagina_inicial/pagina_inicial.html";
+    } else {
+        mensagem.textContent = "Usuário ou senha incorretos!";
+    }
+}
